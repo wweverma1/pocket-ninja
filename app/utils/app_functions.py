cc7fbe10ad4710a3e7b386fc64f57f39c146b6bd
wweverma1 <wweverma1@gmail.com>
2026-01-13 01:57:49 +0900

    using jst timezone in app_functions file

--- app/utils/app_functions.py
+++ app/utils/app_functions.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 from flask import request
 from app import app
+from app.utils.timezone import JST_TZ
 
 
 @app.before_request
@@ -15,6 +16,6 @@ def after_request(response):
     if request.endpoint:
         end_time = time.time()
         latency = int((end_time - request.start_time) * 1000)
-        print(f'[ {str(datetime.now())}] endpoint {request.endpoint} latency {latency} \
+        print(f'[ {str(datetime.now(JST_TZ))}] endpoint {request.endpoint} latency {latency} \
                req_id {request.environ.get("FLASK_REQUEST_ID")}')
     return response
