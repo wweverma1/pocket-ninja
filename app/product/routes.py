6a232ea62d5bd899f1b8a584a42b7e28eb8ff5cb
wweverma1 <wweverma1@gmail.com>
2026-01-18 19:08:11 +0900

    marking get product details endpoint as post

--- app/product/routes.py
+++ app/product/routes.py
@@ -11,6 +11,6 @@ product_endpoints = Blueprint('product', __name__, url_prefix="/product")
 product_endpoints.add_url_rule(
     rule='/', view_func=add_or_update_product_details, methods=['PUT'])
 product_endpoints.add_url_rule(
-    rule='/', view_func=get_product_details, methods=['GET'])
+    rule='/', view_func=get_product_details, methods=['POST'])
 product_endpoints.add_url_rule(
     rule='/list', view_func=get_all_products, methods=['GET'])
